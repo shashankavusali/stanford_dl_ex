c62c74fb6e0c44391355fff90b5585353f9919c1
shashank17 <shashank.avusali@gmail.com>
2018-01-31 23:37:09 -0600

    implementing logistic regression to classifiy MNIST dataset

--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -15,10 +15,19 @@ function [f,g] = logistic_regression(theta, X,y)
 
 
   %
-  % TODO:  Compute the objective function by looping over the dataset and summing
-  %        up the objective values for each example.  Store the result in 'f'.
+  % Compute the objective function by looping over the dataset and summing
+  % up the objective values for each example.  Store the result in 'f'.
+  
+  sigmoid = @(x) (1/(1+exp(-x)));
+  
+  for i = 1:m
+      f = f + (y(i)*log(sigmoid(theta'*X(:,i)))+ (1-y(i))*log(1-sigmoid(theta'*X(:,i))));
+  end
+  f = -f;
   %
-  % TODO:  Compute the gradient of the objective by looping over the dataset and summing
-  %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
-  %
-%%% YOUR CODE HERE %%%
+  % Compute the gradient of the objective by looping over the dataset and summing
+  % up the gradients (df/dtheta) for each example. Store the result in 'g'.
+  
+  for i = 1:m
+      g = g + X(:,i)*(sigmoid(theta'*X(:,i)) - y(i));
+  end
